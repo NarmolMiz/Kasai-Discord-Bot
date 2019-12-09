0df9ddce87b927d4f6676b6ab5483998ea56d506
NarmolMiz <58323347+NarmolMiz@users.noreply.github.com>
2019-12-09 18:01:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = '/'
+
+client.on('ready', () => {
+  console.log(`Bot has been planted, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  client.user.setActivity(`En route sur ${client.guilds.size} serveurs`)
+});
+
+//--------------------------------------------------------
+
+client.login(process.env.TOKEN);
