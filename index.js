0b648c81e936ca14c0f0e2ecb49db1bf4fb3c839
NarmolMiz <58323347+NarmolMiz@users.noreply.github.com>
2019-12-10 19:57:16 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = '/'
 
+const activities_list = [
+  "with the /help command.",
+  "with the developers console",
+  "with some code",
+  "with JavaScript"
+];
+
 client.on('ready', () => {
+
   console.log(`Bot has been planted, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   client.user.setActivity(`En route sur ${client.guilds.size} serveurs`)
-});
 
-client.on('message', async message => {
-	if (message.content === `${prefix}ping`) {
-           const m = await message.channel.send("Ping?");
-           m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-           console.log('Used command : ping')
+  setInterval(() => {
+    const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
+    client.user.setActivity(activities_list[index]);
+  }, 60000);
 });
 
 //--------------------------------------------------------
