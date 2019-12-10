889423e0aea41ee23781c8f5a6d0c7a6c369c7a1
NarmolMiz <58323347+NarmolMiz@users.noreply.github.com>
2019-12-10 20:37:45 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ client.on('ready', () => {
 
 client.on("message", (message) => {
   if (message.content.startsWith(`${prefix}kick`)) {
-    var member = message.mentions.members.first();
-    member.kick().then((member) => {
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    let member = args[0];
+    let reason = args[1];
+    member.kick(reason).then((member) => {
       message.channel.get('653993368405409831').send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
-    }).catch(() => {
-      message.channel.send("Access Denied");
     });
   }
 });
@@ -36,3 +36,18 @@ client.on("message", (message) => {
 //----------------------------------------------
 
 client.login(process.env.TOKEN);
+
+
+//-----------------------------------------------
+
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+
+if (command === "asl") {
+  let age = args[0]; // Remember arrays are 0-based!.
+  let sex = args[1];
+  let location = args[2];
+  message.reply(`Hello ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
+}
+
+https://anidiots.guide/first-bot/command-with-arguments
