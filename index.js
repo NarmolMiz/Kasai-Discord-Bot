b1502d9542fc84c36e2c5557d22caa751b6b7724
NarmolMiz <58323347+NarmolMiz@users.noreply.github.com>
2019-12-12 18:13:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ client.on("message", async message => {
     // To get the "message" itself we join the `args` back into a string with spaces: 
     const sayMessage = args.join(" ");
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(🤔 => { });
+    message.delete().catch(O_o => { });
     // And we get the bot to say the thing: 
     message.channel.send(sayMessage);
   }
