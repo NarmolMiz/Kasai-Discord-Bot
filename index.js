936337e3090198133646a2939dd78a6399ee051f
NarmolMiz <58323347+NarmolMiz@users.noreply.github.com>
2019-12-13 19:19:14 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -111,121 +111,6 @@ client.on("message", async message => {
     message.channel.get('653993368405409831').send(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
   }
 
-});
-
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const prefix = '/'
-
-//-------------------------------------------------
-
-const activities_list = [
-  "with the /help command.",
-  "with the developers console",
-  "with some code",
-  "with JavaScript"
-];
-
-//--------------------------------------------
-
-client.on('ready', () => {
-
-  console.log(`Bot has been planted, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-  client.user.setActivity(`En route sur ${client.guilds.size} serveurs`)
-
-  setInterval(() => {
-    const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
-    client.user.setActivity(activities_list[index]);
-  }, 60000);
-});
-//--------------------------------------------------------
-
-client.on("guildCreate", guild => {
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-});
-
-//----------------------------------------------------------
-
-client.on("guildDelete", guild => {
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-});
-
-//--------------------------------------------------------
-
-client.on("message", async message => {
-
-  if (message.author.bot) return;
-
-  if (message.content.indexOf(prefix) !== 0) return;
-
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-
-  if (message.content.startsWith(`${prefix}ping`)) {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
-
-  if (message.content.startsWith(`${prefix}say`)) {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o => { });
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
-  }
-
-  if(message.content.startsWith(`${prefix}kick`)) {
-    // This command must be limited to mods and admins. In this example we just hardcode the role names.
-    // Please read on Array.some() to understand this bit: 
-    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    
-    // Let's first check if we have a member and if we can kick them!
-    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-    // We can also support getting the member by ID, which would be args[0]
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-    
-    // slice(1) removes the first part, which here should be the user mention or ID
-    // join(' ') takes all the various parts to make it a single string.
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
-    
-    // Now, time for a swift kick in the nuts!
-    await member.kick(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.channel.get('653993368405409831').send(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
-
-  }
-  
-  if(message.content.startsWith(`${prefix}ban`)) {
-    // Most of this command is identical to kick, except that here we'll only let admins do it.
-    // In the real world mods could ban too, but this is just an example, right? ;)
-    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    
-    let member = message.mentions.members.first();
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.bannable) 
-      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
-
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
-    
-    await member.ban(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-    message.channel.get('653993368405409831').send(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
-  }
-
   if (message.content.startsWith(`${prefix}clean`)) {
     // This command removes all messages from all users in the channel, up to 100.
 
@@ -241,15 +126,8 @@ client.on("message", async message => {
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
-  
+
 });
-//----------------------------------------------
-
-client.login(process.env.TOKEN);
-
-
-//-----------------------------------------------
-
 //----------------------------------------------
 
 client.login(process.env.TOKEN);
